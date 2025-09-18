152f16067b9240567bae9875b8d270efb7cc54a2
alihasna32 <muktermukter555@gmail.com>
2025-09-18 23:37:12 +0600

    content

--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -4,11 +4,16 @@ export default function Countries({ countriesPromise }) {
   const countriesData = use(countriesPromise);
   const countries = countriesData.countries;
   return (
-    <div>
-      <h1>In the countries: {countries.length}</h1>
-      {countries.map((country) => (
-        <Country country={country}></Country>
-      ))}
+    <div className="mx-5">
+      <h1 className="text-3xl font-semibold">
+        In the countries: {countries.length}
+      </h1>
+
+      <div className="grid grid-cols-3 gap-5">
+        {countries.map((country) => (
+        <Country key={country.name.common} country={country}></Country>
+        ))}
+      </div>     
     </div>
   );
 }
--- src/components/country/country.jsx
+++ src/components/country/country.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export default function country() {
+export default function Country({ country }) {
+  const [visited, setVisited] = useState(false);
+  const [nonVisited, setNonVisited] = useState(false);
+
+  const handleVisited = () => {
+    setVisited(true);
+    setNonVisited(false);
+  };
+
+  const handleNonVisited = () => {
+    setNonVisited(true);
+    setVisited(false);
+  };
   return (
-    <div><h3></h3></div>
-  )
+    <div className="p-2.5 rounded-xl shadow-xl border border-gray-200">
+      <img
+        className="mb-4 w-full rounded-2xl"
+        src={country.flags.flags.png}
+        alt={country.flags.flags.alt}
+      />
+      <h3>Name: {country.name.common}</h3>
+      <p>Population: {country.population.population}</p>
+      <p>
+        Status:
+        {country.area.area > 300000 ? " Big Country" : " Small Country"}
+      </p>
+      <div className="flex gap-4 mt-5">
+        <button
+          className="p-3 rounded-xl border border-gray-300"
+          onClick={handleVisited}
+        >
+          {visited ? "Visited🟢" : "Visited"}
+        </button>
+
+        <button
+          className="p-3 rounded-xl border border-gray-300"
+          onClick={handleNonVisited}
+        >
+          {nonVisited ? "Non Visited🟢" : "Non-Visited"}
+        </button>
+      </div>
+    </div>
+  );
 }

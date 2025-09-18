713a058e186d4016935fb6280eea2c46b5d1a730
alihasna32 <muktermukter555@gmail.com>
2025-09-18 20:11:08 +0600

    content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
-function App() {
+import { Suspense } from "react";
+import Countries from "./components/countries/countries";
 
+const countriesPromise = fetch(
+  "https://openapi.programming-hero.com/api/all"
+).then((res) => res.json());
+
+function App() {
   return (
     <>
-     
+      <Suspense fallback={<p>waiting...</p>}>
+        <Countries countriesPromise={countriesPromise} />
+      </Suspense>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -1,6 +1,11 @@
-export default function Countries() {
+import { use } from "react";
+export default function Countries({countriesPromise}) {
+    const countries = use(countriesPromise)
+    console.log(countries)
     return(
-
-    )
+        <>
+            <h1>HElLO</h1>
+        </>
+    );
 }
 
